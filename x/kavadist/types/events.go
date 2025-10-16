04e46d4365f6e7d9cbadae4f96d790639e483208
jhon09851 <jhon_blockchain@outlook.com>
2025-10-16 14:52:45 +0800

    refactor(kavadist/types): migrate expected keeper interfaces from istdist

---
+++ x/kavadist/types/events.go
@@ -0,0 +1,8 @@
+package types
+
+const (
+	EventTypeKavaDist      = ModuleName
+	AttributeKeyInflation  = "kava_dist_inflation"
+	AttributeKeyStatus     = "kava_dist_status"
+	AttributeValueInactive = "inactive"
+)
